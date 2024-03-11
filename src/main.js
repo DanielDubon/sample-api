16e642f533a105b950f7b330246c534e36472530
Daniel <dub22233@uvg.edu.gt>
2024-03-10 19:52:55 -0600

    feat: endpoints, cors, added

--- src/main.js
+++ src/main.js
@@ -1,9 +1,75 @@
 import express from 'express'
+import fs from 'fs';
 import { getAllPosts , createPost, getPostById, updatePost, deletePost} from './db.js'
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Express API para Blog',
+    version: '1.0.0',
+    description: 'Esta es la documentación de la API para mi aplicación de Blog.',
+  },
+  servers: [
+    {
+      url: 'http://localhost:3000',
+      description: 'Servidor de desarrollo',
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+
+  apis: ['./routes/*.js', './schemas/*.js'], 
+};
+
+const swaggerSpec = swaggerJsdoc(options);
 
 const app = express()
 app.use(express.json())
+  
+
+
+
+
+
+
+app.use((req, res, next) => {
+  const oldWrite = res.write;
+  const oldEnd = res.end;
+  const chunks = [];
+
+  res.write = function (chunk) {
+    chunks.push(chunk);
+    return oldWrite.apply(res, arguments);
+  };
+
+  res.end = function (chunk) {
+    if (chunk) {
+      chunks.push(chunk);
+    }
+    const body = Buffer.concat(chunks).toString('utf8');
+    const logEntry = `${new Date().toISOString()} - ${req.method} ${req.url} - Request Payload: ${JSON.stringify(req.body)} - Response: ${body}\n`;
+
+
+    fs.appendFile('log.txt', logEntry, (err) => {
+      if (err) throw err;
+    });
+
+    oldEnd.apply(res, arguments);
+  };
+
+  next();
+});
+
+
+
+
+
+
+
 
 app.get('/posts', async (req, res) => {
     const posts = await getAllPosts()
@@ -21,7 +87,8 @@ app.post('/posts', async (req, res) => {
       if (result.affectedRows && result.affectedRows > 0) {
           res.status(201).json({ message: 'Post created successfully', postId: result.insertId })
       } else {
-          res.status(404).json({ message: 'Failed to create post' })
+          res.status(404
+            ).json({ message: 'Failed to create post' })
       }
   } catch (error) {
       res.status(500).json({ message: 'Server error while creating post', error: error.message })
@@ -83,6 +150,12 @@ app.delete('/posts/:postId', async (req, res) => {
   }
 })
 
+
+
+
+app.use((req, res, next) => {
+  res.status(501).json({ message: "Not Implemented" });
+});
 
 const port = 3000
 
