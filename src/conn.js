9b2d22ac26cd698316f5306db8816b5eaa8a8350
SergioAle210 <ore221122@uvg.edu.gt>
2024-03-19 00:41:12 +0000

    fix: api fixed

--- src/conn.js
+++ src/conn.js
@@ -2,9 +2,9 @@ import mysql from 'mysql2/promise'
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'dubon',
-  database: 'blog_dubon',
+  user: 'blog_dubon',
   password: 'dubon',
+  database: 'blog_dubon',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
